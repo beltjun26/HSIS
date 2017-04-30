3c1d4af2737a7c5c7d93621b9f7d6b646e50f159
beltjun26 <beltjunabisado@gmail.com>
2017-04-30 13:25:35 +0800

    Add navigation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(Auth::guest()){
+            return view('/login');
+        }else{
+            return view('home');            
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
